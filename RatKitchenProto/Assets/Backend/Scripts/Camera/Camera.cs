7552eab809cb20d21e1af30736162083c2c348ff
@Spexuz <FilipChristianPerneklo@Gmail.com>
2025-11-10 15:51:58 +0100

    God Save my soul

--- RatKitchenProto/Assets/Backend/Scripts/Camera/Camera.cs
+++ RatKitchenProto/Assets/Backend/Scripts/Camera/Camera.cs
@@ -1,21 +1,55 @@
+using System.Collections;
 using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
+    [Header("Camera Dependencies")]
+    public GameObject player;
+    public PlayerMovement playerScript;
+    [Header("Camera Movement Settings")]
     public float moveSpeed = 5f;
     public Vector3 direction = Vector3.forward;
-
-    public GameObject player;
-    public GameObject anchorPoint;
-
+    [Header("Camera Chase Settings")] 
+    [SerializeField] private float ChaseSpeed;
+    private float ChaseThreshold;
+    public float ChaseCooldownTime = 2f;
+    private bool CanChase;
+    private bool IsChasing;
 
     private void Start()
     {
-
+        ChaseThreshold = transform.position.z - player.transform.position.z;
     }
 
     public void UpdateCamera()
     {
-        transform.position += direction * moveSpeed * Time.deltaTime;
+        transform.position += direction * moveSpeed * Time.deltaTime; //Camera Consistant Movement
+        StartCoroutine(CameraChasePlayer());                   //Camera Chase Routine
+    }
+
+    public IEnumerator CameraChasePlayer()
+    {
+        while (true)
+        {
+            if (!playerScript.isGrounded) //Check if player has been grounded for X amount of time.
+            {
+                CanChase = false;
+                yield return new WaitForSeconds(ChaseCooldownTime);
+                CanChase = true;
+            }
+
+            if (CanChase)
+            {
+                if (player.transform.position.z + ChaseThreshold > transform.position.z) //Is Player ahead of Camera?
+                {
+                    IsChasing = true;
+                    Vector3 TargetPosition = transform.position;
+                    TargetPosition.z = player.transform.position.z + ChaseThreshold;
+                    
+                    //Smooth Chase Movement
+                    transform.position = Vector3.Lerp(transform.position, TargetPosition, ChaseSpeed * Time.deltaTime);
+                }
+            } yield return null; 
+        }
     }
 }
