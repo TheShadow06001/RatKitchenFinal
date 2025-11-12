63bdd32b856b9b27451fe73c77285de1bc2e6d5f
Clovexx <bossestrom2@gmail.com>
2025-11-12 13:27:34 +0100

    MixerVariables

--- RatKitchenProto/Assets/JoelleScripts/EnemyDamage.cs
+++ RatKitchenProto/Assets/JoelleScripts/EnemyDamage.cs
@@ -3,8 +3,7 @@ using UnityEngine.UI;
 
 public class EnemyDamage : MonoBehaviour
 {
-    public GameObject PlayerHeartDisplay;
-    public int damage = 2;
+   
      
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,8 +21,7 @@ public class EnemyDamage : MonoBehaviour
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            // todo: add health script
-            //PlayerHeartDisplay.TakeDamage(damage);
+            HeartDisplay.instance.TakeDamage();
         }
     }
 }
